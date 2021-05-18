7d0b827e740af8b55657c70106bd442ab5a336e8
meleciosilva <mpsilva94@gmail.com>
2021-05-17 22:54:00 -0400

    update app-level middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 
+const eventsRouter = require("./events/events.router");
+const notFound = require("./errors/notFound");
+const errorHandler = require("./errors/errorHandler");
 
+app.use(express.json());
+
+app.use("/events", eventsRouter);
+
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
