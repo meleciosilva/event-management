d2a0a1c9ba9339a2a991ae494038fded2797d799
meleciosilva <mpsilva94@gmail.com>
2021-05-18 21:16:04 -0400

    add mongodb atlas url

--- src/database/setup.js
+++ src/database/setup.js
@@ -1,15 +1,21 @@
-// setup mongoose
+require("dotenv").config();
+// set up mongoose
 const mongoose = require("mongoose");
-const connectionString = "mongodb://localhost:27017/events";
+
+// const connectionString = "mongodb://localhost:27017/bookstore";
+
+const url = `mongodb+srv://mel-silva:${process.env.DB_PASS}@mycluster.7bu2d.mongodb.net/bookstore?retryWrites=true&w=majority`;
 
 // connect application to database
 module.exports = () => {
-  mongoose.connect(connectionString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
+  mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
   }, (err) => {
-    if (err) return console.error(err);
+    if (err) return console.log(err);
     console.log("Database Connection is Successful!");
-  });
+    }
+  );
 }
