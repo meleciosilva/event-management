00d16b2a3de950c33e53ff45e943be9f8fcb8575
meleciosilva <mpsilva94@gmail.com>
2021-05-23 09:19:15 -0400

    add /auth route

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 
-const eventsRouter = require("./events/events.router");
+const authRouter = require("./routers/auth.router");
+const eventsRouter = require("./routers/events.router");
 const notFound = require("./errors/notFound");
 const errorHandler = require("./errors/errorHandler");
 
 app.use(express.json());
 
+app.use("/auth", authRouter);
 app.use("/events", eventsRouter);
 
 app.use(notFound);
