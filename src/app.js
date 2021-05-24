a9af7f3edb4a58f190011e12c26aa184f7a4af05
meleciosilva <mpsilva94@gmail.com>
2021-05-24 17:20:35 -0400

    add seedAdmin

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
 
+// ROUTERS
 const authRouter = require("./routers/auth.router");
 const eventsRouter = require("./routers/events.router");
 const notFound = require("./errors/notFound");
 const errorHandler = require("./errors/errorHandler");
 
+// SEEDERS
+const { seedAdmin } = require("./seeders/admin");
+// seedAdmin()
+
+// APP-MIDDLEWARE
 app.use(express.json());
 
 app.use("/auth", authRouter);
